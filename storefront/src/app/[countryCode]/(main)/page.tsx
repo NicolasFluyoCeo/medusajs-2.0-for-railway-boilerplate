a35168b36ce0bead22dd372b64637fc511a56d7f
Nicolas Rondon <nicolas@fluyo.co>
2025-02-16 11:43:17 -0500

    sotre

--- storefront/src/app/[countryCode]/(main)/page.tsx
+++ storefront/src/app/[countryCode]/(main)/page.tsx
@@ -18,21 +18,16 @@ export default async function Home({
 }: {
   params: { countryCode: string }
 }) {
-  const collections = await getCollectionsWithProducts(countryCode)
-  const region = await getRegion(countryCode)
-
-  if (!collections || !region) {
-    return null
-  }
+.022
 
   return (
     <>
       <Hero />
-      <div className="py-12">
+      {/* <div className="py-12">
         <ul className="flex flex-col gap-x-6">
           <FeaturedProducts collections={collections} region={region} />
         </ul>
-      </div>
+      </div> */}
       <TrainingAd />
       <BestSellerSection />
     </>
